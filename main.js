6f346e2a71e73115437101f2cb5ba574be2ebb76
Nils Ekman <nils.ekman@consultant.husqvarnagroup.com>
2019-11-12 23:49:44 +0100

    Named exports with export default is not working

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-import SamsungDiscovery from './src/samsung-discovery';
+import Discovery from './src/samsung-discovery';
 import * as utils from './src/utils';
 
+// https://github.com/standard-things/esm/issues/182
+export const SamsungDiscovery = Discovery;
 export const Utils = utils;
 
 export default SamsungDiscovery;
+
